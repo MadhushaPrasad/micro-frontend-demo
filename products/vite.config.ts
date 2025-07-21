f1adf18065ac2a3f07c61b68e3386cc2a1889d92
Madhusha Prasad <madushaprasad21@gmail.com>
2025-07-21 15:46:22 +0530

    feat: Add module federation configuration to Vite for FeaturedProductsList component

--- products/vite.config.ts
+++ products/vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { federation } from '@module-federation/vite'
+import { dependencies } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    federation({
+      filename: 'remoteEntry.js',
+      name: 'products',
+      exposes: {
+        './FeaturedProductsList': './src/components/FeaturedProductsList/FeaturedProductsList.tsx',
+      },
+      remotes: {},
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: dependencies.react,
+        }
+      }
+    }),
+    react()
+  ],
 })
