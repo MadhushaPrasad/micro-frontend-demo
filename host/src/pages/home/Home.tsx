0ed6f2d30ed794830869dc7fd5221d0937974f87
Madhusha Prasad <madushaprasad21@gmail.com>
2025-07-21 16:09:23 +0530

    feat: Add initial App component and styling, including Home page with lazy-loaded FeaturedProducts

---
+++ host/src/pages/home/Home.tsx
@@ -0,0 +1,22 @@
+import React, { Suspense } from "react";
+
+
+const FeaturedProducts = React.lazy(
+   // @ts-ignore
+   async () => import('products/FeaturedProductsList'),
+);
+
+
+const Home = () => {
+ return (
+   <div>
+     <h2>Home</h2>
+     <Suspense fallback="loading...">
+       <FeaturedProducts />
+     </Suspense>
+   </div>
+ );
+};
+
+
+export default Home;
